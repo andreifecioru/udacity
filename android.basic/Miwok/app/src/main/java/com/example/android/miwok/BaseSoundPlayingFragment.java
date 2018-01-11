2b17028e778df95992cf23a694e8acd274d2b755
Andrei Fecioru <afecioru@adobe.com>
2018-01-11 18:50:47 +0200

    [ANDROID.BASIC] - Using nav-drawer in the 'Miwok' app.

--- android.basic/Miwok/app/src/main/java/com/example/android/miwok/BaseSoundPlayingFragment.java
+++ android.basic/Miwok/app/src/main/java/com/example/android/miwok/BaseSoundPlayingFragment.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.View;
--- android.basic/Miwok/app/src/main/java/com/example/android/miwok/MainActivity.java
+++ android.basic/Miwok/app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -1,17 +1,34 @@
 package com.example.android.miwok;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+
+import butterknife.BindArray;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    @BindView(R.id.drawer_layout) DrawerLayout mDrawerLayout;
+    @BindView(R.id.drawer_list_view) ListView mDrawerListView;
+    @BindArray(R.array.nav_items) String[] mNavItemsArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,19 +36,78 @@ class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
-        if (viewPager != null) {
-            PagerAdapter pagerAdapter = new MiwokTabsPagerAdapter(getSupportFragmentManager());
-            viewPager.setAdapter(pagerAdapter);
+        ButterKnife.bind(this);
 
-            TabLayout tabLayout = (TabLayout) findViewById(R.id.pager_tabs);
-            tabLayout.setupWithViewPager(viewPager);
+        mDrawerListView.setAdapter(new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                mNavItemsArray));
+
+        mDrawerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectItem(position);
+            }
+        });
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.drawer_open, R.string.drawer_close);
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
         }
+
+        selectItem(0);
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void selectItem(int position) {
+        Fragment fragment;
+
+        switch (position) {
+            case 0: fragment = new NumbersFragment(); break;
+            case 1: fragment = new ColorsFragment(); break;
+            case 2: fragment = new FamilyFragment(); break;
+            case 3: fragment = new PhrasesFragment(); break;
+            default: fragment = null;
+        }
+
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame, fragment)
+                    .commit();
+
+            mDrawerListView.setItemChecked(position, true);
+            setTitle(mNavItemsArray[position]);
+            mDrawerLayout.closeDrawer(Gravity.LEFT);
+        }
     }
 }
