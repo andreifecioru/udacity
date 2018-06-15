0985b0ff80d2e1521589d26298295bd1679b3799
Andrei Fecioru <afecioru@adobe.com>
2018-06-15 14:20:22 +0300

    [ANDROID.DEV] - Lesson 08 | Exercise: 03

--- android.developer/exercises/Lesson08-Quiz-Example/T08.03-Exercise-FinishQuizExample/app/src/main/java/com/udacity/example/quizexample/MainActivity.java
+++ android.developer/exercises/Lesson08-Quiz-Example/T08.03-Exercise-FinishQuizExample/app/src/main/java/com/udacity/example/quizexample/MainActivity.java
@@ -23,6 +23,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.udacity.example.droidtermsprovider.DroidTermsExampleContract;
 
@@ -39,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
     private int mCurrentState;
 
     private Button mButton;
+    private TextView mWordTextView;
+    private TextView mDefinitionTextView;
 
     // This state is when the word definition is hidden and clicking the button will therefore
     // show the definition
@@ -55,12 +58,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Get the views
-        // TODO (1) You'll probably want more than just the Button
-        mButton = (Button) findViewById(R.id.button_next);
+        mButton = findViewById(R.id.button_next);
+        mWordTextView = findViewById(R.id.text_view_word);
+        mDefinitionTextView = findViewById(R.id.text_view_definition);
 
         //Run the database operation to get the cursor off of the main thread
         new WordFetchTask().execute();
-
     }
 
     /**
@@ -87,27 +90,34 @@ public class MainActivity extends AppCompatActivity {
         // Change button text
         mButton.setText(getString(R.string.show_definition));
 
-        // TODO (3) Go to the next word in the Cursor, show the next word and hide the definition
-        // Note that you shouldn't try to do this if the cursor hasn't been set yet.
-        // If you reach the end of the list of words, you should start at the beginning again.
         mCurrentState = STATE_HIDDEN;
+        mDefinitionTextView.setVisibility(View.INVISIBLE);
 
+        if (mData != null) {
+            if (!mData.moveToNext()) mData.moveToFirst();
+
+            String word = mData.getString(mData.getColumnIndex(DroidTermsExampleContract.COLUMN_WORD));
+            mWordTextView.setText(word);
+        }
     }
 
     public void showDefinition() {
 
         // Change button text
         mButton.setText(getString(R.string.next_word));
-
-        // TODO (4) Show the definition
         mCurrentState = STATE_SHOWN;
 
+        if (mData == null) return;
+
+        String definition = mData.getString(mData.getColumnIndex(DroidTermsExampleContract.COLUMN_DEFINITION));
+        mDefinitionTextView.setText(definition);
+        mDefinitionTextView.setVisibility(View.VISIBLE);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        // TODO (5) Remember to close your cursor!
+        mData.close();
     }
 
     // Use an async task to do the data fetch off of the main thread.
@@ -136,8 +146,7 @@ public class MainActivity extends AppCompatActivity {
             // Set the data for MainActivity
             mData = cursor;
 
-            // TODO (2) Initialize anything that you need the cursor for, such as setting up
-            // the screen with the first word and setting any other instance variables
+            nextWord();
         }
     }
 
