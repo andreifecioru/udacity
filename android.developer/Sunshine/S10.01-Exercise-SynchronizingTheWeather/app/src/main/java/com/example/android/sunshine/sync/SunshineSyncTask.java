fd328f6ae7cb9e3cd2d33c04cce98c13756e3700
Andrei Fecioru <andrei.fecioru@gmail.com>
2018-07-24 22:36:39 +0300

    [ANDROID.DEV] - Sunshine 10 | Exercise: 01

--- android.developer/Sunshine/S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
+++ android.developer/Sunshine/S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
@@ -1,4 +1,38 @@
-//  TODO (1) Create a class called SunshineSyncTask
-//  TODO (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
-//      TODO (3) Within syncWeather, fetch new weather data
-//      TODO (4) If we have valid results, delete the old data and insert the new
+package com.example.android.sunshine.sync;
+
+import java.net.URL;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+
+import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
+import com.example.android.sunshine.data.WeatherContract.WeatherEntry;
+
+
+class SunshineSyncTask {
+    synchronized public static void syncWeather(Context context) {
+        URL weatherRequestUrl = NetworkUtils.getUrl(context);
+
+        try {
+            String jsonWeatherResponse = NetworkUtils
+                    .getResponseFromHttpUrl(weatherRequestUrl);
+
+            ContentValues[] weatherData = OpenWeatherJsonUtils
+                    .getWeatherContentValuesFromJson(context, jsonWeatherResponse);
+
+            if (weatherData != null && weatherData.length != 0) {
+                ContentResolver resolver = context.getContentResolver();
+
+                // delete everything
+                resolver.delete(WeatherEntry.CONTENT_URI, null, null);
+
+                // bulk-insert the new data
+                resolver.bulkInsert(WeatherEntry.CONTENT_URI, weatherData);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
