3703942a1630449e7d89f8e9a21cca92768bf6b2
Andrei Fecioru <andrei.fecioru@gmail.com>
2018-10-20 12:51:24 +0300

    [ANDROID.DEV] - Emojify | Extracting emoji from face

--- android.developer/Emojify/app/src/main/java/com/example/android/emojify/Emojifier.java
+++ android.developer/Emojify/app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -10,8 +10,32 @@ import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.face.Face;
 import com.google.android.gms.vision.face.FaceDetector;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Emojifier {
     private static final String LOG_TAG = Emojifier.class.getSimpleName();
+
+    private static final float IS_SMILING_THRESHOLD = 0.5f;
+    private static final float IS_LEFT_EYE_OPEN_THRESHOLD = 0.5f;
+    private static final float IS_RIGHT_EYE_OPEN_THRESHOLD = 0.5f;
+
+    private static final Map<ImmutableFaceCharacteristics, Integer> emojiTable = new HashMap<>();
+
+    static {
+        emojiTable.put(ImmutableFaceCharacteristics.builder().isSmiling(true).isLeftEyeOpen(true).isRightEyeOpen(true).build(), R.drawable.smile);
+        emojiTable.put(ImmutableFaceCharacteristics.builder().isSmiling(true).isLeftEyeOpen(true).isRightEyeOpen(false).build(), R.drawable.rightwink);
+
+        emojiTable.put(ImmutableFaceCharacteristics.builder().isSmiling(true).isLeftEyeOpen(false).isRightEyeOpen(true).build(), R.drawable.leftwink);
+        emojiTable.put(ImmutableFaceCharacteristics.builder().isSmiling(true).isLeftEyeOpen(false).isRightEyeOpen(false).build(), R.drawable.closed_smile);
+
+        emojiTable.put(ImmutableFaceCharacteristics.builder().isSmiling(false).isLeftEyeOpen(true).isRightEyeOpen(true).build(), R.drawable.frown);
+        emojiTable.put(ImmutableFaceCharacteristics.builder().isSmiling(false).isLeftEyeOpen(true).isRightEyeOpen(false).build(), R.drawable.rightwinkfrown);
+
+        emojiTable.put(ImmutableFaceCharacteristics.builder().isSmiling(false).isLeftEyeOpen(false).isRightEyeOpen(true).build(), R.drawable.leftwinkfrown);
+        emojiTable.put(ImmutableFaceCharacteristics.builder().isSmiling(false).isLeftEyeOpen(false).isRightEyeOpen(false).build(), R.drawable.closed_frown);
+    }
+
     static void detectFaces(Context context, Bitmap bitmap) {
         // Create the face detector, disable tracking and enable classifications
         FaceDetector detector = new FaceDetector.Builder(context)
@@ -34,7 +58,8 @@ class Emojifier {
         } else {
             Toast.makeText(context, "Faces: " + faces.size(), Toast.LENGTH_SHORT).show();
             for (int i =0; i < faces.size(); i++) {
-                getClassifications(faces.valueAt(i));
+                Integer emojiResId = whichEmoji(faces.valueAt(i));
+                Log.d(LOG_TAG, "Emoji res id: " + emojiResId);
             }
         }
 
@@ -42,12 +67,23 @@ class Emojifier {
         detector.release();
     }
 
-    private static void getClassifications(Face face) {
-        // Log all the probabilities
-        Log.d(LOG_TAG, "getClassifications: smilingProb = " + face.getIsSmilingProbability());
-        Log.d(LOG_TAG, "getClassifications: leftEyeOpenProb = "
-                + face.getIsLeftEyeOpenProbability());
-        Log.d(LOG_TAG, "getClassifications: rightEyeOpenProb = "
-                + face.getIsRightEyeOpenProbability());
+    private static Integer whichEmoji(Face face) {
+        boolean isSmiling = face.getIsSmilingProbability() > IS_SMILING_THRESHOLD;
+        boolean isLeftEyeOpen = face.getIsLeftEyeOpenProbability() > IS_LEFT_EYE_OPEN_THRESHOLD;
+        boolean isRightEyeOpen = face.getIsRightEyeOpenProbability() > IS_RIGHT_EYE_OPEN_THRESHOLD;
+
+        ImmutableFaceCharacteristics characteristics = ImmutableFaceCharacteristics.builder()
+            .isSmiling(isSmiling)
+            .isLeftEyeOpen(isLeftEyeOpen)
+            .isRightEyeOpen(isRightEyeOpen).build();
+
+        return emojiTable.get(characteristics);
+    }
+
+    @org.immutables.value.Value.Immutable()
+    public interface FaceCharacteristics {
+        boolean isSmiling();
+        boolean isLeftEyeOpen();
+        boolean isRightEyeOpen();
     }
 }
