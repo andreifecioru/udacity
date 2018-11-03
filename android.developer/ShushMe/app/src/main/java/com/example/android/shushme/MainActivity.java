275a8070ebf951b30770ceb49dcc3377ea0f8c31
Andrei Fecioru <andrei.fecioru@gmail.com>
2018-11-03 12:43:48 +0200

    [ANDROID.DEV] - ShushMe | Create and configure a GoogleAPI client

--- android.developer/ShushMe/app/src/main/java/com/example/android/shushme/MainActivity.java
+++ android.developer/ShushMe/app/src/main/java/com/example/android/shushme/MainActivity.java
@@ -16,19 +16,46 @@ package com.example.android.shushme;
 * limitations under the License.
 */
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Places;
+
+public class MainActivity
+        extends AppCompatActivity
+        implements GoogleApiClient.ConnectionCallbacks,
+                   GoogleApiClient.OnConnectionFailedListener {
 
     // Constants
-    public static final String TAG = MainActivity.class.getSimpleName();
+    public static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
 
     // Member variables
     private PlaceListAdapter mAdapter;
     private RecyclerView mRecyclerView;
+    private CheckBox mLocationPermissionsCheckBox;
+    private LinearLayout mLocationPermissionsContainer;
+    private Button mAddPlaceButton;
+    private GoogleApiClient mGoogleApiClient;
 
     /**
      * Called when the activity is starting
@@ -41,11 +68,101 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Set up the recycler view
-        mRecyclerView = (RecyclerView) findViewById(R.id.places_list_recycler_view);
+        mRecyclerView = findViewById(R.id.places_list_recycler_view);
+        mLocationPermissionsCheckBox = findViewById(R.id.location_permissions);
+        mLocationPermissionsContainer = findViewById(R.id.location_permissions_container);
+        mAddPlaceButton = findViewById(R.id.add_place_button);
+
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mAdapter = new PlaceListAdapter(this);
         mRecyclerView.setAdapter(mAdapter);
 
+        mLocationPermissionsCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.d(LOG_TAG, "Location permissions changed: " + isChecked);
+                if (isChecked) {
+                    askForLocationPermissions();
+                }
+            }
+        });
+
+        mAddPlaceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = (isLocationPermissionGranted())
+                        ? "Location permissions granted."
+                        : "Location permissions NOT granted.";
+
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mLocationPermissionsCheckBox.setChecked(false);
+
+        // TODO (4) Create a GoogleApiClient with the LocationServices API and GEO_DATA_API
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .addApi(Places.GEO_DATA_API)
+                .enableAutoManage(this, this)
+                .build();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        int visibilityStatus = isLocationPermissionGranted() ? View.GONE : View.VISIBLE;
+        mLocationPermissionsContainer.setVisibility(visibilityStatus);
+    }
+
+    private boolean isLocationPermissionGranted() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void askForLocationPermissions() {
+        if (!isLocationPermissionGranted()) {
+            ActivityCompat.requestPermissions(this,
+                    new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+                    REQUEST_LOCATION_PERMISSION);
+        } else {
+            mLocationPermissionsContainer.setVisibility(View.GONE);
+            // TODO: get location
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_LOCATION_PERMISSION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Location permissions granted", Toast.LENGTH_SHORT).show();
+                    mLocationPermissionsContainer.setVisibility(View.GONE);
+                    // TODO: get location
+                } else {
+                    Toast.makeText(this, "Location permissions NOT granted", Toast.LENGTH_SHORT).show();
+                    mLocationPermissionsContainer.setVisibility(View.VISIBLE);
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.i(LOG_TAG, "API Connection Successful!");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(LOG_TAG, "API Connection Suspended!");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i(LOG_TAG, "API Connection Failed!");
+    }
 }
